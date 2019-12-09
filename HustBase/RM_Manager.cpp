12d785eb829aa45f5b01c0af87e65a03de67c3de
Xuming Chen <994459124@qq.com>
2019-12-09 23:38:46 +0800

    Update RM_Manager.cpp

--- HustBase/RM_Manager.cpp
+++ HustBase/RM_Manager.cpp
@@ -33,8 +33,44 @@ RC UpdateRec (RM_FileHandle *fileHandle,const RM_Record *rec)
 	return SUCCESS;
 }
 
-RC RM_CreateFile (char *fileName, int recordSize)
+// 创建一个名为fileName的记录文件，该文件中每条记录的大小为recordSize
+RC RM_CreateFile(char *fileName, int recordSize)
 {
+	RC rc;
+
+	// 打开文件，获取页面句柄
+	PF_FileHandle pfFileHandle;
+
+	if ((rc = CreateFile(fileName)))
+	{
+		return rc;
+	}
+	if ((rc = openFile(fileName, &pfFileHandle)))
+	{
+		return rc;
+	}
+
+	// 根据页面句柄生成页面信息控制页(第1页)
+	PF_PageHandle pfPageHandle;
+
+	if ((rc = AllocatePage(&pfFileHandle, &pfPageHandle)))
+	{
+		return rc;
+	}
+	assert(pfPageHandle.pFrame->page.pageNum == 1);
+
+	// 从pData[0]开始，存放一个数据结构RM_FileSubHeader记录控制信息，并进行初始化
+	RM_FileSubHeader *rmFileSubHeader;
+
+	rmFileSubHeader = (RM_FileSubHeader *)pfPageHandle.pFrame->page.pData;
+	rmFileSubHeader->nRecords = 0;
+	rmFileSubHeader->recordSize = recordSize;
+	rmFileSubHeader->recordsPerPage = (PF_PAGE_SIZE - sizeof(RM_FileSubHeader)) / recordSize;
+	if ((rc = MarkDirty(&pfPageHandle)) || (rc = UnpinPage(&pfPageHandle)) || (rc = CloseFile(&pfFileHandle)))
+	{
+		return rc;
+	}
+
 	return SUCCESS;
 }
 RC RM_OpenFile(char *fileName, RM_FileHandle *fileHandle)
